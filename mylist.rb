d81bec37dfc937ab400bb99d7d86f94506a1f6bd
Besufekad Alemu <besurye@gmail.com>
2023-05-18 16:53:09 +0300

    Add mylist.rb file to operate the my enumberable function and update the readme

---
+++ mylist.rb
@@ -0,0 +1,31 @@
+require_relative 'myenumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each
+    count = 0
+    while count < @list.length
+      yield @list[count] if block_given?
+      count += 1
+    end
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+
+puts(list.all? { |e| e > 5 })
+
+# Test #any?
+puts(list.any? { |e| e == 2 })
+
+puts(list.any? { |e| e == 5 })
+
+# Test #filter
+print(list.filter(&:even?))
